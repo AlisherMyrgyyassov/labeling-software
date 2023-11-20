90e4d190af0378b4b2930e7db1717f6413cfcc08
AlisherMyrgyyassov <alister_m2001@mail.ru>
2023-11-21 02:13:16 +0800

    fixed bugs and updates the program

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import cv2
 import json
 
 import os
+import numpy as np
 
 with open('data.json', 'r') as file: data = json.load(file)
 
@@ -40,7 +41,7 @@ def update_circles_on_canvas():
             canvas.delete(item_id)
 
     # Draw the circles
-    for item_id in tree.get_children():
+    for i, item_id in enumerate(tree.get_children()):
         # Get the coordinates from the second column
         coords = tree.item(item_id)['values'][1]
 
@@ -48,9 +49,10 @@ def update_circles_on_canvas():
         if coords:
             x, y = map(int, coords.split())
             x, y = normalize_to_event(x, y)
-            # Draw a red circle on the canvas at the specified coordinates
+
             r = 5
             canvas.create_oval(x - r, y - r, x + r, y + r, fill='red')
+            canvas.create_text(x, y - r - 10, text=str(i+1), fill="red", font=('bold'))
 
 def normalize_to_event(abs_x, abs_y):
     # normalize the coordinates with respect to the resized image on the canvas
@@ -72,13 +74,37 @@ def normalize_to_absolute(event_x, event_y):
 
     return abs_coord_x, abs_coord_y
 
+def auto_pick_landmarks():
+    global current_picture
+    global current_folder
+    file_path = os.path.join(current_folder, current_picture)
+
+    img = cv2.imread(file_path)
+    faces = detector.detectMultiScale(img)
+    #face_coordinates = faces[0]
+
+    if len(faces) >= 1:
+        # Detect landmarks of the biggest
+        index, box = max(enumerate(faces), key=lambda x: x[1][2])
+        _, landmarks = landmark_detector.fit(img, np.array([faces[index].tolist()]))
+
+        selected_indices = [18, 21, 22, 26, 36, 39, 42, 45, 0, 30, 16, 48, 54, 8]
+        selected_landmarks = [landmarks[0][0][i] for i in selected_indices]
+        for i, item in enumerate(tree.get_children()):
+            coords = selected_landmarks[i]
+            # Convert to integers and then to string
+            text = ' '.join(map(str, map(int, map(round, coords))))
+            # Update the second column of the item
+            tree.set(item, '#2', text)
+        update_circles_on_canvas()
+
 current_picture = ""
 current_folder = ""
 current_image_id = ""
 
 is_resized = False
-x_len = 320
-y_len = 300
+x_len = 360
+y_len = 310
 
 orig_x = 0
 orig_y = 0
@@ -88,6 +114,10 @@ orig_y = 0
 haarcascade = "haarcascade_frontalface_alt2.xml"
 detector = cv2.CascadeClassifier(haarcascade)
 
+# https://www.google.com/url?sa=i&url=https%3A%2F%2Fanswers.opencv.org%2Fquestion%2F192167%2Ffacemarklbf-training-fewer-landmark-points-than-68%2F&psig=AOvVaw1Qt5otMj4qtC0pPcrgD6g7&ust=1700545351471000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiow57v0YIDFQAAAAAdAAAAABAE
+LBFmodel = "LFBmodel.yaml"
+landmark_detector  = cv2.face.createFacemarkLBF()
+landmark_detector.loadModel(LBFmodel)
 
 app = tk.Tk()
 app.title("Image Viewer")
@@ -115,7 +145,7 @@ tree.place(relx=0.6, rely = 0.07, relwidth=0.2, relheight=0.86)
 tree.column('name', width=50)
 tree.column('coordinates', width=100)
 
-# take all valuse except for the addresses
+# take all values except for the addresses
 for value in data["header"][1:]:
     tree.insert('', 'end', values=(value, '', '', ''))
 
@@ -131,10 +161,14 @@ def save_data ():
 save_button = tk.Button(app, text = "Save data", command=save_data)
 save_button.place(relx=0.6, rely = 0.93, relwidth=0.2, relheight=0.04)
 
+auto_pick_button = tk.Button(app, text = "Auto", command=auto_pick_landmarks)
+auto_pick_button.place(relx=0.9, rely = 0.03, relwidth=0.05, relheight=0.03)
+
+
 
 frame_listbox = tk.Frame(app, background="gray") 
 frame_listbox.place(relx = 0.81, rely = 0.07, relheight=0.9, relwidth=0.14)
-images_listbox = Listbox (frame_listbox, selectmode="single")
+images_listbox = Listbox (frame_listbox, selectmode="single", exportselection=0)
 images_listbox.place(relwidth = 1, relheight = 1)
 
 listbox_scrollbar = tk.Scrollbar(frame_listbox)
@@ -152,6 +186,33 @@ def on_select(evt):
 
 images_listbox.bind('<<ListboxSelect>>', on_select)
 
+
+def go_to_next_line():
+    selected = images_listbox.curselection()
+    if selected:
+        next_index = selected[0] + 1
+
+        if next_index < images_listbox.size():
+            images_listbox.selection_clear(selected)
+            images_listbox.selection_set(next_index)
+            images_listbox.see(next_index)
+            images_listbox.event_generate("<<ListboxSelect>>")
+
+def go_to_previous_line():
+    selected = images_listbox.curselection()
+    if selected:
+        prev_index = selected[0] - 1
+        if prev_index >= 0:
+            images_listbox.selection_clear(selected)
+            images_listbox.selection_set(prev_index)
+            images_listbox.see(prev_index)
+            images_listbox.event_generate("<<ListboxSelect>>")
+
+next_button = tk.Button(app, text=">", command=go_to_next_line)
+next_button.place(relx = 0.56, rely=0.45, relheight=0.1, relwidth=0.03)
+
+prev_button = tk.Button(app, text="<", command=go_to_previous_line)
+prev_button.place(relx = 0.01, rely=0.45, relheight=0.1, relwidth=0.03)
 
 
 # Create a canvas to display the images
@@ -197,12 +258,25 @@ canvas.bind("<Button-1>", on_click)
 
 
 
+
+
 def select_image():
     # Open file dialog to select an image file
     file_path = filedialog.askopenfilename()
 
     if file_path:
         open_image (file_path)
+    
+        # UPDATE LISTBOX
+        global current_folder
+        file_names = os.listdir(current_folder)
+        # Perform a natural sort on the file names
+        sorted_file_names = sorted(file_names, key=lambda x: int(os.path.splitext(x)[0].split('_')[1]))
+        images_listbox.delete(0, tk.END)
+        for file_name in sorted_file_names:
+            images_listbox.insert(tk.END, file_name)
+            
+
         select_item_in_listbox(images_listbox, file_path.split("/")[-1])
         
 
@@ -221,15 +295,10 @@ def open_image(file_path):
     idx = f"{last_folder}/{current_picture}"
     update_tree_from_json (idx)
 
-    file_names = os.listdir(folder_path)
-    # Perform a natural sort on the file names
-    sorted_file_names = sorted(file_names, key=lambda x: int(os.path.splitext(x)[0].split('_')[1]))
-    images_listbox.delete(0, tk.END)
-    for file_name in sorted_file_names:
-        images_listbox.insert(tk.END, file_name)
-
+    
     faces = detector.detectMultiScale(cv2.imread(file_path))
-    face_coordinates = faces[0]
+    print (faces)
+    
     # getting values in this format [[280  46 210 210]]
     # (640, 480) = (x, y)
 
@@ -240,7 +309,11 @@ def open_image(file_path):
     orig_x = 0
     orig_y = 0
 
-    if len(faces) == 1:
+    if len(faces) >= 1:
+        # Get the index of the box with the biggest area for zoom
+        index, box = max(enumerate(faces), key=lambda x: x[1][2])
+        face_coordinates = faces[index]
+        
         is_resized = True
         center_x = int(face_coordinates[0] + face_coordinates[2]/2)
         center_y = int(face_coordinates[1] + face_coordinates[3]/2)
@@ -263,6 +336,7 @@ def open_image(file_path):
         print ("cropped with the coordinates", bbox)
 
         print("Faces:\n", face_coordinates)
+    
         
 
     #orig_size = image.size
